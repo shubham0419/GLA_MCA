6ac384c64db53c591ea3402eaa364c00e06ca103
Your Name <you@example.com>
2024-07-23 16:00:01 +0530

    traval add

--- Sec-A/travel/script.js
+++ Sec-A/travel/script.js
@@ -11,23 +11,46 @@ document.getElementById('mobile-menu').addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+const usersDestinations = [];
 
 const date = document.getElementById("dateAdd");
+const destinationSelect = document.getElementById("destination");
+const hotelContainer = document.getElementById("hotelContainer")
+
+destinationSelect.addEventListener("change",(e)=>{
+    const hotelInput = document.createElement("select");
+    hotelInput.setAttribute("id","hotel");
+    hotelInput.setAttribute("name","hotel");
+    const availableHotels = destinations.find((destination)=>destination.name==e.target.value);
+    availableHotels.hotels.forEach(hotel=>{
+        const hotelOption = `<option value="${hotel.name}">${hotel.name}</option>`;
+        hotelInput.innerHTML+=hotelOption;
+    })
+    hotelContainer.innerHTML = "";
+    hotelContainer.appendChild(hotelInput);
+})
 
 console.log(date);
 const selectedDestinaton = document.getElementById("destination");
 
-date.addEventListener("change",(e)=>{
-    const input  = document.createElement("select");
-    input.setAttribute("id", "hotel");
-    input.setAttribute("name", "hotel");
-    const destination = selectedDestinaton.value;
+const form = document.getElementById("new-schedule-form");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const destination = destinations.find((destination)=>destination.name==e.target.destination.value);
     console.log(destination);
-    const place = destinations.find(place)
-    for(let i=0;i<destination.length;i++){
-        let option = `<option value="${destination[i].name}">${destination[i].name}</option>`;
-        input.innerHTML += option;
-    }
-    selectedDestinaton.after(input);
-    console.log(selectedDestinaton);
+    const div =  document.createElement('div');
+    const image = `<img src="${destination.Image[0]} alt="${destination.name}"`;
+    const name = `<h3>${destination.name}</h3>`
+    const hotel = `<h5>${e.target.hotel.value}</h5>`
+    const date = `<p>${e.target.date.value}</p>`
+    const info = `<p>${destination.information}</p>`
+    console.log(image);
+    div.innerHTML += image;
+    console.log(div);
+    div.innerHTML += name;
+    div.innerHTML += hotel;
+    div.innerHTML += date;
+    div.innerHTML += info;
+    console.log(div);
 })
