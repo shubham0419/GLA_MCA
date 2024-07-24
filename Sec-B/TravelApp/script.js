7b9f3b40edf0ae7a7dd6e4f9d3c2439a27af141c
Your Name <you@example.com>
2024-07-24 15:02:26 +0530

    ok

--- Sec-B/TravelApp/script.js
+++ Sec-B/TravelApp/script.js
@@ -22,15 +22,59 @@ destinationSelect.addEventListener("change", (e) => {
     const hotelSelect = document.createElement("select");
     // todo - give styling to select by classList.add
     hotelSelect.id = "hotel-select";
-    hotelSelect.name = "hotel-select";
+    hotelSelect.name = "hotelSelect";
     for(let hotel of hotels){
         const option = `<option value="${hotel.name}">${hotel.name}</option>`;
         hotelSelect.insertAdjacentHTML("beforeend", option);
       }
     console.log(hotelSelect);
-    const label = `<label for="hotel-select">Selct hotel : </label>`;
+    const label = `<label for="hotel-select">Select hotel : </label>`;
     hotelSelector.innerHTML = "";
     hotelSelector.insertAdjacentHTML("beforeend", label);
     hotelSelector.insertAdjacentHTML("beforeend", hotelSelect.outerHTML);
   }
 });
+
+const destinationForm = document.getElementById("new-schedule-form");
+const scheduleItems = document.getElementById("schedule-items");
+
+destinationForm.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  const {destination,date,hotelSelect} = e.target;
+
+  const div = document.createElement("div");
+
+  const foundDestination = destinations.find(
+    (singleDestination) => singleDestination.name == destination.value
+  );
+
+  const image = document.createElement("img");
+  image.src = foundDestination.Image[0];
+  image.alt = foundDestination.name;
+  
+  const name = `<h3>${foundDestination.name}</h3>`;
+  const hotel = `<h4>Selected hotel : ${hotelSelect.value}</h4>`
+  const dateEle = `<p>Bookig date : ${date.value}</p>`;
+  const info = `<p>${foundDestination.information}</p>`
+  div.classList.add("schedule-item");
+
+  const divInside = document.createElement("div");
+  divInside.style.display = "flex"
+  divInside.style.gap = "10px"
+
+  divInside.append(image);
+  
+  const divInsideInside = document.createElement("div");
+  divInsideInside.innerHTML += name;
+  divInsideInside.innerHTML += hotel;
+  divInsideInside.innerHTML += dateEle;
+
+  divInside.append(divInsideInside);
+
+  div.append(divInside)
+  div.innerHTML += info;
+  scheduleItems.prepend(div);
+})
+
+
