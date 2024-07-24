f308a0ed2b67ba35e6ef8f8ca4bd67374b5c4c28
Your Name <you@example.com>
2024-07-24 12:59:32 +0530

    add destination done

--- Sec-A/travel/script.js
+++ Sec-A/travel/script.js
@@ -22,7 +22,7 @@ destinationSelect.addEventListener("change", (e) => {
     const hotelSelect = document.createElement("select");
     // todo - give styling to select by classList.add
     hotelSelect.id = "hotel-select";
-    hotelSelect.name = "hotel-select";
+    hotelSelect.name = "hotelSelect";
     for(let hotel of hotels){
         const option = `<option value="${hotel.name}">${hotel.name}</option>`;
         hotelSelect.insertAdjacentHTML("beforeend", option);
@@ -34,3 +34,33 @@ destinationSelect.addEventListener("change", (e) => {
     hotelSelector.insertAdjacentHTML("beforeend", hotelSelect.outerHTML);
   }
 });
+
+
+const destinationForm = document.getElementById("new-schedule-form");
+const scheduleItems = document.getElementById("schedule-items");
+
+destinationForm.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  const {destination,hotelSelect,date} = e.target;
+
+  const destinationFound = destinations.find((dest)=>dest.name==destination.value);
+    console.log(destinationFound)
+    const div =  document.createElement('div');
+    const img =  document.createElement('img');
+    img.setAttribute("src",destinationFound.Image[0]);
+    img.setAttribute("alt",destinationFound.name);
+    const name = `<h3>${destinationFound.name}</h3>`
+    const hotel = `<h5>${hotelSelect.value}</h5>`
+    const dateEle = `<p>${date.value}</p>`
+    const info = `<p>${destinationFound.information}</p>`
+    div.append(img);
+    div.innerHTML += name;
+    div.innerHTML += hotel;
+    div.innerHTML += dateEle;
+    div.innerHTML += info;
+    scheduleItems.append(div);
+});
+
+
+
